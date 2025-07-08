92de0dd4a0da50262b8d43aac06ea4d8f4e119e6
saraz28 <saramoha288@gmail.com>
2025-07-08 20:14:24 +0300

    add primeng

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { KnobModule } from 'primeng/knob';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, KnobModule, FormsModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
