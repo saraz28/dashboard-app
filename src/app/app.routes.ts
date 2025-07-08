405e667fccbde5501c235fc6de8ba0888f6d37f5
saraz28 <saramoha288@gmail.com>
2025-07-08 20:06:47 +0300

    create new component and add lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard').then((mod) => {
+        console.log('Lazy loading Dashboard component...');
+
+        return mod.Dashboard;
+      }),
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
